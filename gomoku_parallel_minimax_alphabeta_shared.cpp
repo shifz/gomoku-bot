7155fcfb6406cbc1692a24856f057c90255292e0
Jiaxin Ye <jiaxinye@JiaxindeMacBook-Pro.local>
2020-11-21 20:43:03 -0500

    minimax shared

--- gomoku_parallel_minimax_alphabeta_shared.cpp
+++ gomoku_parallel_minimax_alphabeta_shared.cpp
@@ -47,39 +47,29 @@ int evaluate_states(vector<vector<int> >& board, int depth, bool maxplayer, int 
 pair<int, int> minimax_parallel_distributed(vector<vector<int> >& board, int depth, int alpha, int beta, bool debug){
     vector<pair<int,int> > moves;
     get_moves(board,moves);
-    int global_best_value = INT_MIN;
-    pair<int, int> global_best_move = moves[0];
+    int best_value = INT_MIN;
+    pair<int, int> best_move = moves[0];
     #pragma omp parallel
     {
-        int local_best_value = INT_MIN;
-        pair<int, int> local_best_move = moves[0];
-        int local_alpha = alpha;
-        int local_beta = beta;
         vector<vector<int> > local_board = board;
 
         #pragma omp for schedule(static)
         for (size_t i = 0; i < moves.size(); i++){
             int value = 0;
             local_board[moves[i].first][moves[i].second] = 1;
-            value = evaluate_states(local_board, depth - 1, false, local_alpha, local_beta, debug);
-            if (value > local_best_value){
-                local_best_value = value;
-                local_best_move = moves[i];
-                local_alpha = local_best_value;
+            value = evaluate_states(local_board, depth - 1, false, alpha, beta, debug);
+            #pragma omp critical
+            {
+                if (value > best_value){
+                    best_value = value;
+                    best_move = moves[i];
+                    alpha = best_value;
+                }
             }
             local_board[moves[i].first][moves[i].second] = -1;
         }
-
-        #pragma omp critical
-        {
-            if (local_best_value > global_best_value){
-                global_best_value = local_best_value;
-                global_best_move = local_best_move;
-            }
-        }
-
     }
-    return global_best_move;
+    return best_move;
 }
 
 pair<int,int> search_next_move(vector<vector<int> >& board, int depth){
